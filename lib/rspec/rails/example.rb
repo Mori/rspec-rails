64d6486d6ca20c57b7fee4a1fa7e3ead0567aca1
Mordecai Kopel <github@isher.com>
2012-01-21 19:07:56 -0700

    Simplify /lib/rspec/rails/example.rb

--- lib/rspec/rails/example.rb
+++ lib/rspec/rails/example.rb
@@ -1,38 +1,22 @@
 require 'rspec/rails/example/rails_example_group'
-require 'rspec/rails/example/controller_example_group'
-require 'rspec/rails/example/request_example_group'
-require 'rspec/rails/example/helper_example_group'
-require 'rspec/rails/example/view_example_group'
-require 'rspec/rails/example/mailer_example_group'
-require 'rspec/rails/example/routing_example_group'
-require 'rspec/rails/example/model_example_group'
+ 
+EXAMPLE_GROUPS = {
+  :controller => :controllers
+  :helper     => :helpers
+  :mailer     => :mailers
+  :model      => :models
+  :request    => '(requests|integration)'
+  :routing    => :routing
+  :view       => :views
+}  
+
+EXAMPLE_GROUPS.each { |k, v| require "rspec/rails/example/#{k}_example_group" }
 
 RSpec::configure do |c|
-  def c.escaped_path(*parts)
-    Regexp.compile(parts.join('[\\\/]'))
-  end
-
-  c.include RSpec::Rails::ControllerExampleGroup, :type => :controller, :example_group => {
-    :file_path => c.escaped_path(%w[spec controllers])
-  }
-  c.include RSpec::Rails::HelperExampleGroup, :type => :helper, :example_group => {
-    :file_path => c.escaped_path(%w[spec helpers])
-  }
-  if defined?(RSpec::Rails::MailerExampleGroup)
-    c.include RSpec::Rails::MailerExampleGroup, :type => :mailer, :example_group => {
-      :file_path => c.escaped_path(%w[spec mailers])
+  EXAMPLE_GROUPS.each do |k, v|
+    next unless defined? group = Rspec::Rails.const_get("#{k.capitalize}ExampleGroup") 
+    c.include group, :type => :controller, :example_group => {
+      :file_path => Regexp.compile(%W[spec #{v}].join('[\\\/]'))
     }
   end
-  c.include RSpec::Rails::ModelExampleGroup, :type => :model, :example_group => {
-    :file_path => c.escaped_path(%w[spec models])
-  }
-  c.include RSpec::Rails::RequestExampleGroup, :type => :request, :example_group => {
-    :file_path => c.escaped_path(%w[spec (requests|integration)])
-  }
-  c.include RSpec::Rails::RoutingExampleGroup, :type => :routing, :example_group => {
-    :file_path => c.escaped_path(%w[spec routing])
-  }
-  c.include RSpec::Rails::ViewExampleGroup, :type => :view, :example_group => {
-    :file_path => c.escaped_path(%w[spec views])
-  }
 end
